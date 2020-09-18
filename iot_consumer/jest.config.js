be9eeb33508ca637cfa22e6b47f6319b01921cb1
rafael <rafael.rac.mg@gmail.com>
2020-09-18 14:23:30 -0300

    chore: jest config

--- iot_consumer/jest.config.js
+++ iot_consumer/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+    // '<rootDir>/src/**/*.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
